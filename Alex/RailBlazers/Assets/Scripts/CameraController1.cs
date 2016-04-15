115d4d03afe31c4c3f9217c84a1ff5201cbed69a
FriantAlex <friant.alex@gmail.com>
2016-04-15 12:02:46 -0400

    Camera movement updated
    Added a more dynamic camera so we can change the position, distance, and
    rotation when we need. The camera is no longer attached to the spline

---
+++ Alex/RailBlazers/Assets/Scripts/CameraController1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController1 : MonoBehaviour {
+
+	public Transform player;
+	public float dist = 15f;
+	public float currentX;
+	public float currentY;
+	public float rotX;
+	public float rotY;
+
+
+
+	private Camera cam;
+
+
+
+	// Use this for initialization
+	void Awake () {
+	
+		cam = GetComponent<Camera> ();
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	
+		Vector3 dir = new Vector3 (0, 0, -dist);
+		Quaternion pos = Quaternion.Euler (currentX, currentY, 0);
+		Quaternion rot = Quaternion.Euler (rotX, rotY, 0);
+		transform.rotation = rot;
+		transform.position = player.position  + pos * dir;
+
+	}
+}
