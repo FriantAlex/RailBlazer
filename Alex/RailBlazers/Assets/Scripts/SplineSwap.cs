381633a507bf66f79ddffb12b448c08c31ae87b6
FriantAlex <friant.alex@gmail.com>
2016-04-18 12:09:07 -0400

    Level 2 added
    Added script to switch spline, made a bunch of different track segments
    for different puzzle rooms. created scene for level 2.

---
+++ Alex/RailBlazers/Assets/Scripts/SplineSwap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineSwap : MonoBehaviour {
+
+	public SplineWalker walker;
+	public BezierSpline curSpline;
+	public BezierSpline newSpline;
+
+	// Use this for initialization
+	void Start () {
+		
+		curSpline = walker.spline;
+	}
+
+
+	void OnTriggerEnter(Collider other){
+
+			walker.spline = newSpline;
+		
+	}
+}
