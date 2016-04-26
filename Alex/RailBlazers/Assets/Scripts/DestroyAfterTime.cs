b0ad82a05e1e613f2e9a7e2332a855cfb610124c
FriantAlex <friant.alex@gmail.com>
2016-04-25 20:10:16 -0400

    fixes
    fixed performance issues and added back in the lights

---
+++ Alex/RailBlazers/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterTime : MonoBehaviour {
+
+
+	public float secs;
+
+
+	void Start(){
+
+		StartCoroutine (B(secs));
+
+	}
+
+	IEnumerator B(float seconds){
+
+		yield return new WaitForSeconds(seconds);
+		Destroy(gameObject);
+	}
+}
